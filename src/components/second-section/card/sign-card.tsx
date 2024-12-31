46b00b6da91aee13eab85b240fcabf96c5a6603f
Gaetan Darrort <gaetan.darrort@epitech.eu>
2024-12-30 23:10:37 -0500

    adding border radius to cards

--- src/components/second-section/card/sign-card.tsx
+++ src/components/second-section/card/sign-card.tsx
@@ -1,7 +1,7 @@
 function SignCard() {
   return (
     <div className="relative flex w-[300px] flex-col items-center justify-center p-4">
-      <div className="bg-background-sign-card h-[300px] w-[200px] bg-cover bg-center bg-no-repeat"></div>
+      <div className="bg-background-sign-card h-[300px] w-[200px] rounded-md bg-cover bg-center bg-no-repeat"></div>
       <p className="absolute -bottom-10 left-2 z-10 font-sans text-[150px] font-bold text-foreground">
         1
       </p>

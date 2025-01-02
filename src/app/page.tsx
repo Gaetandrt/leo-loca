4d890e7b50d9a22e8d35434f6f9fd464a226dc48
Gaetan Darrort <gaetan.darrort@epitech.eu>
2025-01-01 20:21:24 -0500

    Re-organize codebase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-import FirstSection from "@/components/first-section/first-section";
-import SecondSection from "@/components/second-section/second-section";
-import ThirdSection from "@/components/third-section/third-section";
+import FirstSection from "@/components/sections/first-section/first-section";
+import SecondSection from "@/components/sections/second-section/second-section";
+import ThirdSection from "@/components/sections/third-section/third-section";
 
 export default function Home() {
   return (
